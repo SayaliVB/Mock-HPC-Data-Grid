d4a0beb441a7e340e93b21510da0fdaff379f727
SayaliVB <svbayaskar@gmail.com>
2024-04-11 11:19:48 -0700

    Execution time performance

--- simulation-code/cpp-src/src/app/MPI.cpp
+++ simulation-code/cpp-src/src/app/MPI.cpp
@@ -7,7 +7,7 @@
 #include <dirent.h>
 
 
-int verbose = 1;
+int verbose = 3;
 
 struct AirQualityData {
     double latitude;
@@ -29,6 +29,7 @@ std::vector<std::string> find_files(const std::string& directory) {
     std::vector<std::string> filenames;
     DIR *dir;
     struct dirent *ent;
+    /*
     if ((dir = opendir(directory.c_str())) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             std::string filename = ent->d_name;
@@ -40,6 +41,35 @@ std::vector<std::string> find_files(const std::string& directory) {
     } else {
         std::cerr << "Error opening directory: " << directory << std::endl;
     }
+    */
+    
+
+    if ((dir = opendir(directory.c_str())) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_DIR) {
+                // Ignore "." and ".." directories
+                if (std::strcmp(ent->d_name, ".") == 0 || std::strcmp(ent->d_name, "..") == 0) {
+                    continue;
+                }
+                // Recursively search subdirectories
+                std::string subdir = directory + "/" + ent->d_name;
+                std::vector<std::string> subfiles = find_files(subdir);
+                filenames.insert(filenames.end(), subfiles.begin(), subfiles.end());
+            } else {
+                std::string filename = ent->d_name;
+                std::string foldername = directory.substr(directory.find_last_of('/') + 1);
+                if (filename.find(foldername) == 0) {
+                    filenames.push_back(directory + "/" + filename);
+                }
+            }
+        }
+        closedir(dir);
+    } else {
+        std::cerr << "Error opening directory: " << directory << std::endl;
+    }
+    
+    
+    
     return filenames;
 }
 
@@ -69,7 +99,7 @@ std::vector<AirQualityData> read_csv_data(const std::string& filename) {
             entry.latitude = std::stod(token);
         } catch (const std::invalid_argument& e) {
             
-            if (verbose == 2){
+            if (verbose > 2){
                 std::cerr << token << " -> Invalid argument latitude: " << e.what() << std::endl;
             }
             entry.latitude =  0.0; 
@@ -82,7 +112,7 @@ std::vector<AirQualityData> read_csv_data(const std::string& filename) {
             entry.longitude = std::stod(token);
         } catch (const std::invalid_argument& e) {
             
-            if (verbose == 2){
+            if (verbose > 2){
                 std::cerr << token << " -> Invalid argument longitude: " << e.what() << std::endl;
             }
             entry.longitude =  0.0; 
@@ -101,7 +131,7 @@ std::vector<AirQualityData> read_csv_data(const std::string& filename) {
             entry.concentration = std::stod(token);
         } catch (const std::invalid_argument& e) {
             
-            if (verbose == 2){
+            if (verbose > 2){
                 std::cerr << token << " -> Invalid argument concentration: " << e.what() << std::endl;
             }
             entry.concentration =  0.0; 
@@ -117,7 +147,7 @@ std::vector<AirQualityData> read_csv_data(const std::string& filename) {
             entry.rawConcentration = std::stod(token);
         } catch (const std::invalid_argument& e) {
             
-            if (verbose == 2){
+            if (verbose > 2){
                 std::cerr << token << " -> Invalid argument rawConcentration: " << e.what() << std::endl;
             }
             entry.rawConcentration =  0.0; 
@@ -129,7 +159,7 @@ std::vector<AirQualityData> read_csv_data(const std::string& filename) {
             entry.AQI = std::stod(token);
         } catch (const std::invalid_argument& e) {
             
-            if (verbose == 2){
+            if (verbose > 2){
                 std::cerr << token << " -> Invalid argument AQI: " << e.what() << std::endl;
             }
             entry.AQI =  0.0; 
@@ -141,7 +171,7 @@ std::vector<AirQualityData> read_csv_data(const std::string& filename) {
             entry.category = std::stod(token);
         } catch (const std::invalid_argument& e) {
             
-            if (verbose == 2){
+            if (verbose > 2){
                 std::cerr << token << " -> Invalid argument category: " << e.what() << std::endl;
             }
             entry.category =  0.0; 
@@ -178,6 +208,30 @@ double calculate_average(const std::vector<AirQualityData>& data) {
     return total_concentration / data.size();
 }
 
+void execution_time(int rank, double start, double end, int records, int totprocess) {
+    std::string filename = "execution_time_for_" + std::to_string(totprocess) + "_processes.txt";
+    std::ofstream metrics(filename, std::ios::app);
+    
+    // Check if the file opened successfully
+    if (metrics.is_open()) {
+        // Calculate the duration of execution
+        double duration = end - start;
+        
+        // Calculate records processed per second
+        double recordsPerSecond = records / duration;
+        
+        // Write the metrics to the file
+        metrics << "Process Rank: " << rank << ", Records processed: " << records <<", Execution time: " << duration << "s, Records per second: " << recordsPerSecond << std::endl;
+        
+        // Close the file
+        metrics.close();
+    } else {
+        // Print an error message if file opening fails
+        std::cerr << "Failed to open metrics file for process " << rank << std::endl;
+    }
+}
+
+
 int main(int argc, char *argv[]) {
     MPI_Init(NULL, NULL);
 
@@ -192,56 +246,119 @@ int main(int argc, char *argv[]) {
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    std::vector<double> all_partial_averages(size);
+    //std::map<std::string, double> partial_averages;
     double partial_average;
-
     if (rank == 0) {
         // Rank 0 reads the filenames and sends them to other ranks
-        std::vector<std::string> filenames;
 
         std::string directory = argv[1];
         
-        if (verbose == 2){
+        if (verbose > 2){
             std::cout<<"Directory : " <<directory<< std::endl;
         }
+        std::vector<std::string> filenames;
         filenames = find_files(directory);
 
         std::cout << "Rank " << rank << " processed files " << filenames.size()  << std::endl;
 
+        if (verbose >4){
+            for (int i = 0; i < filenames.size(); ++i){
+                std::cout << "File " << i <<" "<< filenames[i]<<std::endl;
+            }
+        }
 
         // Send filenames to other ranks
         for (int i = 1; i < size ; ++i) {
-            //std::cout << "Rank 0 sending file " << filenames[i] << "to  "<< i << std::endl;
-            MPI_Send(filenames[i].c_str(), filenames[i].size() + 1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-            //std::cout << "Rank 0 sent file " << filenames[i] << "to  "<< i << std::endl;
+            MPI_Send(filenames[i-1].c_str(), filenames[i-1].size() + 1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+            if (verbose >3)
+                std::cout << "Rank 0 sent file " << filenames[i-1] << "to  "<< i << std::endl;
         }
+        for (int i = size-1; i < filenames.size(); ++i) {
+            int ranktosend;
+            MPI_Recv(&ranktosend, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (verbose >3)
+                std::cout << "Rank 0 remaining sending file " << filenames[i] << "to  "<< ranktosend << std::endl;
+            MPI_Send(filenames[i].c_str(), filenames[i].size() + 1, MPI_CHAR, ranktosend, 0, MPI_COMM_WORLD);
+        }
+        // Signal termination to other processes
+        for (int i = 1; i < size; ++i) {
+            int ranktosend;
+            MPI_Recv(&ranktosend, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            if (verbose >3)
+                std::cout << "Rank 0 sending empty file to  "<< ranktosend << std::endl;
+            char empty_filename[1] = {'\0'};
+            MPI_Send(empty_filename, 1, MPI_CHAR, ranktosend, 0, MPI_COMM_WORLD);
+            if (verbose >3)
+                std::cout << "Rank 0 sent empty file to  "<< ranktosend << std::endl;
+        }
+
+        
     } else{
-
+        int i =0;
+        int totRecords = 0;
+        auto startTime = std::chrono::high_resolution_clock::now();
         // Receive filenames and process the corresponding files
-        char filename[256];
-        MPI_Recv(filename, 256, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        std::vector<AirQualityData> data = read_csv_data(std::string(filename));
+        while(true){
+            char filename[256];
+            MPI_Recv(&filename, 256, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (verbose> 3 && filename[0] == '\0'){
+                std::cout << " recieved file " << filename << std::endl;
+            }
+            if (filename[0] == '\0') // Termination signal
+            {
+                partial_average /=i;
+                if (verbose >2){
+                    //std::cout << "Rank " << rank << " partial average " << partial_averages.size() << std::endl;
+                    std::cout << "Rank " << rank << " partial average " << partial_average << std::endl;
+                }
+                auto endTime = std::chrono::high_resolution_clock::now();
+                execution_time(rank,  
+                                std::chrono::duration<double>(startTime.time_since_epoch()).count(),  
+                                std::chrono::duration<double>(endTime.time_since_epoch()).count(), 
+                                totRecords, size);
+                break;
+            }
 
-        // Process the data (example: print the number of entries)
-        std::cout << "Rank " << rank << " processed file " << filename << ". Number of entries: "<< data.size() << std::endl;
+            std::vector<AirQualityData> data = read_csv_data(std::string(filename));
 
-        // Calculate partial average
-        partial_average = calculate_average(data);
+            // Process the data (example: print the number of entries)
+            if (verbose>1){
+                std::cout << "Rank " << rank << " processed file " << filename << ". Number of entries: "<< data.size() << std::endl;
+            }
+
+            totRecords+=data.size();
+            // Calculate partial average
+            partial_average += calculate_average(data);
+            
+            
+            MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            i++;
+        }
+
     }
 
-    MPI_Gather(&partial_average, 1, MPI_DOUBLE, all_partial_averages.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
+    std::vector<double> all_partial_averages(size);
+    MPI_Gather(&partial_average, 1, MPI_DOUBLE, all_partial_averages.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // MPI_Send(partial_averages.data(), partial_averages.size(), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+
+    
+    if (rank ==0){
         double total_concentration = 0.0;
-        int total_data_points = 0;
+        int total_data_points = -1;
         for (const auto& avg : all_partial_averages) {
+            if (verbose >2){
+                std::cout << " partial averages " << avg << std::endl;
+            }
+
             total_concentration += avg;
             total_data_points++;
         }
         double final_average = total_concentration / total_data_points;
         std::cout << "Final average concentration: " << final_average << std::endl;
     }
-
+    
     MPI_Finalize();
     return 0;
 }
